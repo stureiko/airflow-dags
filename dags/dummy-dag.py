4ccbb5d5533373a84b34b8370c6240a765683b36
stureiko <stureiko@gmail.com>
2024-04-27 17:32:40 +0300

    remove comments and clean code

--- dags/dummy-dag.py
+++ dags/dummy-dag.py
@@ -1,23 +1,10 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
 from datetime import datetime
 
 args = {
     'owner': 'airflow'
 }
-
-def hello():
-    print('Hello World!')
-
-# with DAG(dag_id="simple_dag_python_operator", schedule_interval='*/1 * * * *',
-#     start_date=datetime(2022, 8, 1), default_args=args, catchup=False) as dag:
-
-#     task1 = PythonOperator(
-#         task_id='task_1',
-#         python_callable=hello)
-
-#     task1
 
 dag = DAG(
     dag_id="btc_analytics",
