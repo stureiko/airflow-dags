cf82e5b51aa0d5786df6ebdca61c0d6022e7e1f5
stureiko <stureiko@gmail.com>
2024-04-27 17:29:07 +0300

    add new task

--- dags/dummy-dag.py
+++ dags/dummy-dag.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators import DummyOperator
 from datetime import datetime
 
 args = {
@@ -9,11 +10,22 @@ args = {
 def hello():
     print('Hello World!')
 
-with DAG(dag_id="simple_dag_python_operator", schedule_interval='*/1 * * * *',
-    start_date=datetime(2022, 8, 1), default_args=args, catchup=False) as dag:
+# with DAG(dag_id="simple_dag_python_operator", schedule_interval='*/1 * * * *',
+#     start_date=datetime(2022, 8, 1), default_args=args, catchup=False) as dag:
 
-    task1 = PythonOperator(
-        task_id='task_1',
-        python_callable=hello)
+#     task1 = PythonOperator(
+#         task_id='task_1',
+#         python_callable=hello)
 
-    task1
+#     task1
+
+dag = DAG(
+    dag_id="btc_analytics",
+    default_args=args,
+    description='DAG btc_analytics - Task 2 Otus DE',
+    start_date=datetime(2021, 7, 1),
+    schedule_interval='*/1 * * * *',
+    catchup=False,
+)
+
+start = DummyOperator(task_id="start", dag=dag)
