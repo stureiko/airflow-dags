24ae7dd29402b0475cb1f58b5c6772e5c1436b14
stureiko <stureiko@gmail.com>
2024-04-27 17:11:04 +0300

    modify to python

--- dags/dummy-dag.py
+++ dags/dummy-dag.py
@@ -1,27 +1,22 @@
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 
 args = {
+    'project_id': 'simple_dag_python_operator',
+    'dag_id': 'simple_dag_python_operator',
     'owner': 'airflow',
     'start_date': datetime(2022, 8, 1),
     'provide_content': True
 }
 
-with DAG(
-    dag_id='dags_dag_python_operator',
-    schedule_interval='*/1 * * * *',
+with DAG(schedule_interval='*/1 * * * *',
     default_args=args,
     catchup=False
 ) as dag:
-    task1 = BashOperator(
-        task_id='task_1',
-        bash_command='echo "Hello world from task 1"'
+    task1 = PythonOperator(
+        task_id='XXXXX',
+        python_callable=lambda: print('Hello World!')
     )
 
-    task2 = BashOperator(
-        task_id='task_2',
-        bash_command='echo "Hello world from task 2"'
-    )
-
-    task1 >> task2
+    task1
