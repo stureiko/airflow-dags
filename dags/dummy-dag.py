ce5b0217a1a08401db6edd9f62cb5d7558cf75c5
stureiko <stureiko@gmail.com>
2024-04-27 17:21:28 +0300

    reformat

--- dags/dummy-dag.py
+++ dags/dummy-dag.py
@@ -3,20 +3,14 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 
 args = {
-    'owner': 'airflow',
-    'start_date': datetime(2022, 8, 1),
-    'provide_content': True
+    'owner': 'airflow'
 }
 
-with DAG(
-    dag_id="simple_dag_python_operator",
-    schedule_interval='*/1 * * * *',
-    default_args=args,
-    catchup=False
-) as dag:
+with DAG(dag_id="simple_dag_python_operator", schedule_interval='*/1 * * * *',
+    start_date=datetime(2022, 8, 1), default_args=args, catchup=False) as dag:
+    
     task1 = PythonOperator(
         task_id='XXXXX',
-        python_callable=lambda: print('Hello World!')
-    )
+        python_callable=lambda: print('Hello World!'))
 
     task1
