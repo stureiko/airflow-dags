48d634fc26d51653ab809ac5792c594c55aa1058
stureiko <stureiko@gmail.com>
2024-04-27 17:15:13 +0300

    add dag_ig

--- dags/dummy-dag.py
+++ dags/dummy-dag.py
@@ -3,14 +3,13 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 
 args = {
-    'project_id': 'simple_dag_python_operator',
     'owner': 'airflow',
     'start_date': datetime(2022, 8, 1),
     'provide_content': True
 }
 
 with DAG(
-    dag_id='simple_dag_python_operator',
+    dag_id="simple_dag_python_operator",
     schedule_interval='*/1 * * * *',
     default_args=args,
     catchup=False
