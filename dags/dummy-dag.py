0f764934ac59c21f19ea676f5b4e91655b640e43
stureiko <stureiko@gmail.com>
2024-04-27 17:23:47 +0300

    add new func hello world

--- dags/dummy-dag.py
+++ dags/dummy-dag.py
@@ -6,11 +6,14 @@ args = {
     'owner': 'airflow'
 }
 
+def hello():
+    print('Hello World!')
+
 with DAG(dag_id="simple_dag_python_operator", schedule_interval='*/1 * * * *',
     start_date=datetime(2022, 8, 1), default_args=args, catchup=False) as dag:
-    
+
     task1 = PythonOperator(
-        task_id='XXXXX',
-        python_callable=lambda: print('Hello World!'))
+        task_id='task_1',
+        python_callable=hello)
 
     task1
